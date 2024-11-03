fc32774970de71c9521b426f1b3e3bcc8d734dbd
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-11-04 02:02:13 +0500

    *скрипт кнопки "вверх" *блок вступление

---
+++ script.js
@@ -0,0 +1,19 @@
+// Показать кнопку при прокрутке вниз
+window.onscroll = function() { toggleScrollButton() };
+
+function toggleScrollButton() {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+// Функция для плавного скролла наверх
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
