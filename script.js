523427f70cbf3f4e1423529e42f8086369ea1e10
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-11-13 20:01:19 +0500

    плавный скролл

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ function scrollToTop() {
     });
 }
 
+        document.querySelectorAll('nav a').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+
